7e1c1cb80eed159f975f542fec08d368e4b663d0
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-05-27 13:12:31 +0200

    hide status.c

--- status.c
+++ status.c
@@ -6,7 +6,7 @@
 /*   By: tbouma <tbouma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:56:23 by tbouma            #+#    #+#             */
-/*   Updated: 2022/05/27 12:16:45 by tbouma           ###   ########.fr       */
+/*   Updated: 2022/05/27 13:11:34 by tbouma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,52 @@
 #include "includes/push_swap.h"
 #include <stdio.h>
 
-void	print_b(t_node **head_node_b)
-{
-	t_node	*temp;
-	int		i;
+// void	print_b(t_node **head_node_b)
+// {
+// 	t_node	*temp;
+// 	int		i;
 
-	temp = *head_node_b;
-	i = ft_list_size(temp);
-	printf("list B\n");
-	printf("length: %d\n", i);
-	if (temp)
-	{
-		while (temp->n)
-		{
-			printf("\tvalue= %d index=%d bucket-N=%d\n",
-				temp->c, temp->i, temp->b_n);
-			if (temp->n != NULL)
-				temp = temp->n;
-		}
-		if (temp)
-			printf("\tvalue= %d index=%d bucket-N=%d\n",
-				temp->c, temp->i, temp->b_n);
-	}
-}
+// 	temp = *head_node_b;
+// 	i = ft_list_size(temp);
+// 	printf("list B\n");
+// 	printf("length: %d\n", i);
+// 	if (temp)
+// 	{
+// 		while (temp->n)
+// 		{
+// 			printf("\tvalue= %d index=%d bucket-N=%d\n",
+// 				temp->c, temp->i, temp->b_n);
+// 			if (temp->n != NULL)
+// 				temp = temp->n;
+// 		}
+// 		if (temp)
+// 			printf("\tvalue= %d index=%d bucket-N=%d\n",
+// 				temp->c, temp->i, temp->b_n);
+// 	}
+// }
 
-void	status_nodes(t_node **head_node_a, t_node **head_node_b)
-{
-	int		i;
-	t_node	*temp;
+// void	status_nodes(t_node **head_node_a, t_node **head_node_b)
+// {
+// 	int		i;
+// 	t_node	*temp;
 
-	temp = *head_node_a;
-	i = ft_list_size(temp);
-	printf("list A\n");
-	printf("length: %d\n", i);
-	if (temp != NULL)
-	{
-		while (temp && temp->n)
-		{
-			printf("\tvalue= %d index=%d bucket-N=%d\n",
-				temp->c, temp->i, temp->b_n);
-			if (temp->n != NULL)
-				temp = temp->n;
-		}
-		if (temp)
-			printf("\tvalue= %d index=%d bucket-N=%d\n",
-				temp->c, temp->i, temp->b_n);
-	}
-	if (*head_node_a != NULL)
-		print_b(head_node_b);
-}
+// 	temp = *head_node_a;
+// 	i = ft_list_size(temp);
+// 	printf("list A\n");
+// 	printf("length: %d\n", i);
+// 	if (temp != NULL)
+// 	{
+// 		while (temp && temp->n)
+// 		{
+// 			printf("\tvalue= %d index=%d bucket-N=%d\n",
+// 				temp->c, temp->i, temp->b_n);
+// 			if (temp->n != NULL)
+// 				temp = temp->n;
+// 		}
+// 		if (temp)
+// 			printf("\tvalue= %d index=%d bucket-N=%d\n",
+// 				temp->c, temp->i, temp->b_n);
+// 	}
+// 	if (*head_node_a != NULL)
+// 		print_b(head_node_b);
+// }
